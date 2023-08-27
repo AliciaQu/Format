f105695ccac102fe2ce831dd39a1952a232d2f94
Jessie <yujiaxin2009@gmail.com>
2023-08-27 15:45:33 +1000

    add js for highline

--- client/index.js
+++ client/index.js
@@ -1,1 +1,14 @@
 import "./index.css";
+
+import { highline } from "@/js/database";
+
+
+const middle = document.querySelector(".main-middle")
+const middle_highline = middle.querySelector(".highline")
+const middle_highline_description = middle_highline.querySelectorAll(".description")
+const middle_highline_description_img = middle_highline.querySelectorAll(".description img")
+const middle_highline_description_p = middle_highline.querySelectorAll(".description p")
+
+middle_highline_description_p.forEach((ele, i) => {
+    ele.textContent = highline.description[i]
+}) 
