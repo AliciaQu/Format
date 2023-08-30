8f9eb3ec09b5ab530d6b4879dc8f0166e368603e
Jessie <yujiaxin2009@gmail.com>
2023-08-30 19:30:00 +1000

    profile be

--- client/index.js
+++ client/index.js
@@ -3,6 +3,21 @@ import "./index.css";
 import { highline, profile } from "@/js/database";
 
 
+//main left
+const main = document.querySelector("main");
+const main_container = main.querySelector(".container")
+
+const left = main_container.querySelector(".main-left")
+const left_profile = left.querySelector(".profile")
+const left_profile_name = left_profile.querySelector("h2")
+const left_profile_at = left_profile.querySelector("p")
+const left_profile_img = left_profile.querySelector("img")
+
+left_profile_name.textContent = profile.name
+left_profile_at.textContent = profile.at
+left_profile_img.src = profile.img_src
+
+//main middle 
 const middle = document.querySelector(".main-middle")
 const middle_highline = middle.querySelector(".highline")
 const middle_highline_description = middle_highline.querySelectorAll(".description")
